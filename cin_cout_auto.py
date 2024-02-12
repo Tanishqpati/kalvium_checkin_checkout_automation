a46e5705c6b07f240007c2a8ff32d1f3d529c4bc
Tanishq Patidar <tanishq.patidar@kalvium.community>
2024-02-12 16:10:18 +0530

    Added code for opening the url in the browser

--- cin_cout_auto.py
+++ cin_cout_auto.py
@@ -5,3 +5,6 @@ def automate_checkin_checkout(action):
     # Specify the URL based on checkin or checkout action
     url = "https://kalvium.community/signal/student_check_in_v1" if action == "checkin" else "https://kalvium.community/signal/student_check_out_v1"
 
+    # Open the URL in the default browser
+    webbrowser.open(url)
+
