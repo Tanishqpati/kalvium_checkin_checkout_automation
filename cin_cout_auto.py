46dbdd2141c43141d41439c7e24e97da9b7d1fad
Tanishq Patidar <tanishq.patidar@kalvium.community>
2024-02-12 16:08:51 +0530

    Created function and added url of checkin and checkout page

--- cin_cout_auto.py
+++ cin_cout_auto.py
@@ -1,3 +1,7 @@
 import webbrowser
 import time
 
+def automate_checkin_checkout(action):
+    # Specify the URL based on checkin or checkout action
+    url = "https://kalvium.community/signal/student_check_in_v1" if action == "checkin" else "https://kalvium.community/signal/student_check_out_v1"
+
